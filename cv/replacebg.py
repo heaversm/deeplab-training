ad6ccc43f78a48ce97db28993beb31514b97617f
Mike Heavers <mheavers@gmail.com>
2020-01-22 09:22:39 -0800

    update replacebg script gitignore cv image directories add automator workflow update photoshop actions for BW mask generation

--- cv/replacebg.py
+++ cv/replacebg.py
@@ -2,6 +2,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import keyboard
+import sys
 
 #RESIZE IMAGE BY WIDTH OR HEIGHT MAINTAINING ASPECT RATIO
 def maintain_aspect_ratio_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
@@ -31,67 +33,138 @@ input_dir = 'input/'
 output_dir = 'output/'
 mask_dir = 'masks/'
 bg_dir = 'bg/'
-
-bg_file = 'track.jpg'
+bg_file = 'space.jpg'
 bg_name = bg_dir + bg_file
 
-#TODO: RUN IN LOOP
-fg_file = '000000000.jpg'
+initial_threshold_val = 150
+threshold_val = initial_threshold_val
 
-fg_name = input_dir + fg_file
-fg_file_no_ext = os.path.splitext(fg_file)[0]
-mask_name = mask_dir + fg_file_no_ext + '.png'
-output_name = output_dir + fg_file_no_ext    + '.png'
+#TODO - make dynamic based on num files in input directory
+num_inputs = 40
 
-bg = cv2.imread(bg_name)
-fg = cv2.imread(fg_name)
-mask = cv2.imread(mask_name)
-mask2 = mask.copy()
-fg2 = fg.copy()
-bg2 = bg.copy()
+output_image = None
 
-#print('Mask Dimensions : ',mask2.shape)
-#print('FG Dimensions : ',fg2.shape)
-#print('BG Dimensions : ',bg.shape)
+def generate_image(iterator,doShow):
+    iteration = None
+    if iterator < 10:
+        iteration = '0' + str(iterator)
+    else:
+        iteration = str(iterator)
 
-#openCV places height before width in shape[] array
+    fg_file = '0000000' + iteration + '.jpg'
 
-bgWidth = bg2.shape[1]
-bgHeight = bg2.shape[0]
+    fg_name = input_dir + fg_file
+    fg_file_no_ext = os.path.splitext(fg_file)[0]
+    mask_name = mask_dir + fg_file_no_ext + '.jpg'
+    output_name = output_dir + fg_file_no_ext    + '.jpg'
 
-fgWidth = fg2.shape[1]
-fgHeight = fg2.shape[0]
+    bg = cv2.imread(bg_name)
+    fg = cv2.imread(fg_name)
+    mask = cv2.imread(mask_name)
+    mask2 = mask.copy()
+    fg2 = fg.copy()
+    bg2 = bg.copy()
 
-#if fgWidth > fgHeight:
-  #fg2 = maintain_aspect_ratio_resize(fg2, height=bgHeight)
-#else:
-#  fg2 = maintain_aspect_ratio_resize(bg2, width=bgWidth)
+    # print('Mask : ',mask2.shape)
+    # print('FG : ',fg2.shape)
+    # print('BG : ',bg.shape)
 
-#resize foreground image proportionally to match background width (should be dynamic to width / height based on ratio)
-fg2 = maintain_aspect_ratio_resize(fg2, width=bgWidth)
-#fg2 = maintain_aspect_ratio_resize(fg2, height=bgHeight)
+    #openCV places height before width in shape[] array
 
-fgWidth = fg2.shape[1]
-fgHeight = fg2.shape[0]
+    bgWidth = bg2.shape[1]
+    bgHeight = bg2.shape[0]
 
-mask2 = maintain_aspect_ratio_resize(mask2, width=fgWidth)
+    fgWidth = fg2.shape[1]
+    fgHeight = fg2.shape[0]
 
-#Blur mask
-th, alpha = cv2.threshold(np.array(mask2),0,255, cv2.THRESH_BINARY)
+    fg2 = maintain_aspect_ratio_resize(fg2, width=bgWidth)
+    mask2 = maintain_aspect_ratio_resize(mask2, width=bgWidth)
 
-#the params for blur ksize must be odd numbers
-alpha = cv2.GaussianBlur(alpha, (21,21),0)
+    fgWidth = fg2.shape[1]
+    fgHeight = fg2.shape[0]
 
-#above a certain rgb value, use the pixels from fg, otherwise use pixels from bg
-bg2[np.where(alpha > 200)] = fg2[np.where(alpha > 200)]
-output = bg2.copy()
+    if fgHeight > bgHeight:
+        fg2 = maintain_aspect_ratio_resize(fg2, height=bgHeight)
+        mask2 = maintain_aspect_ratio_resize(mask2, height=bgHeight)
 
+    #Blur mask
+    #the params for blur ksize must be odd numbers
+    alpha = cv2.GaussianBlur(mask2, (21,21),0)
 
-#SHOW IMAGE
-cv2.imshow('result.png', output)
-cv2.waitKey()
-cv2.destroyAllWindows()
+    #above a certain rgb value, use the pixels from fg, otherwise use pixels from bg
+    
+    # print("MASK:",alpha.shape)
+    # print("FG: ",fg2.shape)
+    # print("BG:",bg2.shape)
+    global threshold_val
+    
+    bg2[np.where(alpha > threshold_val)] = fg2[np.where(alpha > threshold_val)]
+    output = bg2.copy()
 
-#WRITE IMAGE
-#cv2.imwrite( output_name, output );
-print('FINISH: ' + output_name)
+    if doShow:
+        #SHOW IMAGE
+        output_image = cv2.imshow('result.jpg', output)
+        doUpdate = False
+        while True:
+            # The function waitKey waits for a key event infinitely (when delay<=0)
+            k = chr(cv2.waitKey(-1)) 
+            if k == 'x':                       # toggle current image
+                threshold_val-=1
+                print("threshold:",threshold_val)
+                doUpdate = True
+                break
+            elif k == 'z':
+                threshold_val+=1
+                print("threshold:",threshold_val)
+                doUpdate = True
+                break
+            elif k == 'i':
+                iterator+=1
+                print("new image",iterator)
+                threshold_val = initial_threshold_val
+                doUpdate = True
+                break
+            elif k == 's':
+                print("write image")
+                cv2.imwrite( output_name, output )
+                break
+            elif k == 'q':
+                break
+
+        cv2.destroyAllWindows()
+        if doUpdate:
+            generate_image(iterator,True)
+        
+    else:
+        #WRITE IMAGE
+        cv2.imwrite( output_name, output )
+        print('FINISH: ' + output_name)
+
+def generate_all_images(number_to_generate):
+    for i in range(number_to_generate):
+        generate_image(i,False)
+
+def generate_images(start_index,end_index):
+    for i in range(start_index, end_index):
+        generate_image(i,False)
+
+arguments = len(sys.argv) - 1
+
+if arguments > 0:
+    if sys.argv[1] == '--image':
+        generate_image(int(sys.argv[2]),True)
+    elif sys.argv[1] == '--all':
+        #requires currently that you manually adjust num_inputs
+        generate_all_images(num_inputs)
+    elif sys.argv[1] == '--generate':
+        generate_all_images(int(sys.argv[2]))
+    elif sys.argv[1] == '--start':
+        if arguments > 2:
+            if sys.argv[3] == '--end':
+                generate_images(int(sys.argv[2]),int(sys.argv[4]))
+            else: 
+                generate_images(int(sys.argv[2]),num_inputs)
+        else:
+            generate_images(int(sys.argv[2]),num_inputs)
+else:
+    generate_image(0,True)
--- utilities/scrapeImages.py
+++ utilities/scrapeImages.py
@@ -19,14 +19,16 @@ def main(args):
     parser.add_argument('-s', '--search', default='bananas', type=str, help='search term')
     parser.add_argument('-n', '--num_images', default=10, type=int, help='num images to save')
     parser.add_argument('-d', '--directory', default='/Users/gene/Downloads/', type=str, help='save directory')
+    parser.add_argument('-f','--filter', default='isz:m,itp:photo,ic:trans,ift:png', type=str,help='filter string from google advanced search tbs param')
     args = parser.parse_args()
     query = args.search#raw_input(args.search)
+    filters = args.filter
     max_images = args.num_images
     save_directory = args.directory
     image_type="Action"
     query= query.split()
     query='+'.join(query)
-    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch&tbs=isz:m,itp:photo,ic:trans,ift:png"
+    url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch&tbs="+filters
     #header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
     header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36",'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
@@ -38,7 +40,7 @@ def main(args):
     ActualImages=[]# contains the link for Large original images, type of  image
     for a in soup.find_all("div",{"class":"rg_meta"}):
         link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-        ActualImages.append((link,Type))
+    ActualImages.append((link,Type))
     for i , (img , Type) in enumerate( ActualImages[0:max_images]):
         try:
             req = urllib.request.Request(img, headers=header)
